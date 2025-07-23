746b24de9fcb169b2d38226c5f3daf3b1da413a4
Akrit Kumar Behera <akbehera@akrit-cern-pc.dyndns.cern.ch>
2025-07-23 10:18:14 +0200

    Added gcc to build

--- root.sh
+++ root.sh
@@ -3,37 +3,38 @@ version: "v1"
 tag: cms/v6-32-00-patches/69384a6a78
 source: https://github.com/cms-sw/root
 build_requires:
-  - CMake
-  - ninja
+- CMake
+- ninja
 requires:
-  - GSL
-  - libjpeg-turbo
-  - libpng
-  - libtiff
-  - giflib
-  - pcre2
-  - Python
-  - FFTW3
-  - xz
-  - XRootD  
-  - libxml2
-  - zlib
-  - davix
-  - TBB
-  - OpenBLAS
-  - py-numpy
-  - lz4
-  - FreeType
-  - zstd
-  - dcap
+- gcc
+- GSL
+- libjpeg-turbo
+- libpng
+- libtiff
+- giflib
+- pcre2
+- Python
+- FFTW3
+- xz
+- XRootD
+- libxml2
+- zlib
+- davix
+- TBB
+- OpenBLAS
+- py-numpy
+- lz4
+- FreeType
+- zstd
+- dcap
 ---
 case "$(uname)" in
-  Darwin)
-    soext="dylib"
-    ;;
-  *)
-    soext="so"
-    ;;
+Darwin)
+  soext="dylib"
+  ;;
+*)
+  soext="so"
+  ;;
 esac
 PKGBUILDDIR="$BUILDDIR/$PKGNAME-$PKGVERSION"
 
@@ -47,24 +48,24 @@ export CFLAGS=-D__ROOFIT_NOBANNER
 export CXXFLAGS=-D__ROOFIT_NOBANNER
 
 if [ -z "${arch_build_flags:-}" ]; then
-    case "$(uname -m)" in
-        ppc64le)  arch_build_flags="-mcpu=power8 -mtune=power8 --param=l1-cache-size=64 --param=l1-cache-line-size=128 --param=l2-cache-size=512" ;;
-        aarch64)  arch_build_flags="-march=armv8-a -mno-outline-atomics" ;;
-        x86_64)   arch_build_flags="" ;;
-        *)        arch_build_flags="" ;;
-    esac
+  case "$(uname -m)" in
+  ppc64le) arch_build_flags="-mcpu=power8 -mtune=power8 --param=l1-cache-size=64 --param=l1-cache-line-size=128 --param=l2-cache-size=512" ;;
+  aarch64) arch_build_flags="-march=armv8-a -mno-outline-atomics" ;;
+  x86_64) arch_build_flags="" ;;
+  *) arch_build_flags="" ;;
+  esac
 fi
 
 if [ -n "${arch_build_flags:-}" ]; then
-    export CFLAGS="${CFLAGS} ${arch_build_flags}"
-    export CXXFLAGS="${CXXFLAGS} ${arch_build_flags}"
+  export CFLAGS="${CFLAGS} ${arch_build_flags}"
+  export CXXFLAGS="${CXXFLAGS} ${arch_build_flags}"
 fi
 
 # Set LLVM build type based on debug flag
 if [ "${is_debug_build_root_llvm:-}" = "true" ]; then
-    LLVM_BUILD_TYPE="Debug"
+  LLVM_BUILD_TYPE="Debug"
 else
-    LLVM_BUILD_TYPE="Release"
+  LLVM_BUILD_TYPE="Release"
 fi
 
 # Set OS-specific flags
@@ -73,131 +74,131 @@ ARCH=$(uname -m)
 
 # Build CMake command
 cmake_args=(
-    "../$PKGNAME-$PKGVERSION"
-    -G Ninja
-    -DCMAKE_BUILD_TYPE="${cmake_build_type}"
-    -DLLVM_BUILD_TYPE="${LLVM_BUILD_TYPE}"
-    -DCMAKE_INSTALL_PREFIX="${i}"
-    -DCMAKE_C_COMPILER=gcc
-    -DCMAKE_CXX_COMPILER=g++
-    -DCMAKE_Fortran_COMPILER=gfortran
-    -DCMAKE_LINKER=ld
-    -DCMAKE_VERBOSE_MAKEFILE=TRUE
-    -Droot7=ON
-    -Dfail-on-missing=ON
-    -Dgnuinstall=OFF
-    -Droofit=ON
-    -Dvdt=OFF
-    -Dhdfs=OFF
-    -Dqt=OFF
-    -Dtmva=ON
-    -DPython3_EXECUTABLE="${PYTHON3_ROOT}/bin/python3"
-    -Dqtgsi=OFF
-    -Dpgsql=OFF
-    -Dsqlite=OFF
-    -Dmysql=OFF
-    -Doracle=OFF
-    -Dldap=OFF
-    -Dkrb5=OFF
-    -Dftgl=OFF
-    -Dfftw3=ON
-    -Dtbb=ON
-    -Dimt=ON
-    -DFFTW_INCLUDE_DIR="${FFTW3_ROOT}/include"
-    -DFFTW_LIBRARY="${FFTW3_ROOT}/lib/libfftw3.${soext}"
-    -Dmathmore=ON
-    -Dexplicitlink=ON
-    -Dbuiltin_tbb=OFF
-    -Dbuiltin_pcre=OFF
-    -Dbuiltin_freetype=OFF
-    -Dbuiltin_zlib=OFF
-    -Dbuiltin_lzma=OFF
-    -Dbuiltin_gsl=OFF
-    -Dbuiltin_glew=ON
-    -Dbuiltin_ftgl=ON
-    -Dbuiltin_gl2ps=ON
-    -Dbuiltin_afterimage=ON
-    -Dbuiltin_xxhash=ON
-    -Dbuiltin_nlohmannjson=ON
-    -Darrow=OFF
-    -DGSL_ROOT_DIR="${GSL_ROOT}"
-    -DGSL_CBLAS_LIBRARY="${OPENBLAS_ROOT}/lib/libopenblas.${soext}"
-    -DGSL_CBLAS_LIBRARY_DEBUG="${OPENBLAS_ROOT}/lib/libopenblas.${soext}"
-    -DCMAKE_CXX_STANDARD="${cms_cxx_standard}"
-    -Dssl=ON
-    -Dpyroot=ON
-    -Dxrootd=ON
-    -Dbuiltin_xrootd=OFF
-    -DXROOTD_INCLUDE_DIR="${XROOTD_ROOT}/include/xrootd"
-    -DXROOTD_ROOT_DIR="${XROOTD_ROOT}"
-    -DCMAKE_C_FLAGS="-D__ROOFIT_NOBANNER"
-    -DCMAKE_CXX_FLAGS="-D__ROOFIT_NOBANNER"
-    -Dgviz=OFF
-    -Dbonjour=OFF
-    -Dodbc=OFF
-    -Dpythia6=OFF
-    -Dpythia8=OFF
-    -Dfitsio=OFF
-    -Dgfal=OFF
-    -Dchirp=OFF
-    -Dsrp=OFF
-    -Ddavix=ON
-    -Dglite=OFF
-    -Dsapdb=OFF
-    -Dalien=OFF
-    -Dmonalisa=OFF
-    -DJPEG_INCLUDE_DIR="${LIBJPEG_TURBO_ROOT}/include"
-    -DJPEG_LIBRARY="${LIBJPEG_TURBO_ROOT}/lib64/libjpeg.${soext}"
-    -DPNG_INCLUDE_DIRS="${LIBPNG_ROOT}/include"
-    -DPNG_LIBRARY="${LIBPNG_ROOT}/lib/libpng.${soext}"
-    -Dastiff=ON
-    -DTIFF_INCLUDE_DIR="${LIBTIFF_ROOT}/include"
-    -DTIFF_LIBRARY="${LIBTIFF_ROOT}/lib/libtiff.${soext}"
-    -DLIBLZMA_INCLUDE_DIR="${XZ_ROOT}/include"
-    -DLIBLZMA_LIBRARY="${XZ_ROOT}/lib/liblzma.${soext}"
-    -DLZ4_INCLUDE_DIR="${LZ4_ROOT}/include"
-    -DLZ4_LIBRARY="${LZ4_ROOT}/lib/liblz4.${soext}"
-    -DZLIB_ROOT="${ZLIB_ROOT}"
-    -DZLIB_INCLUDE_DIR="${ZLIB_ROOT}/include"
-    -DZSTD_ROOT="${ZSTD_ROOT}"
-    -DCMAKE_PREFIX_PATH="${LZ4_ROOT};${GSL_ROOT};${XZ_ROOT};${GIFLIB_ROOT};${FREETYPE_ROOT};${PYTHON3_ROOT};${LIBPNG_ROOT};${PCRE2_ROOT};${TBB_ROOT};${OPENBLAS_ROOT};${DAVIX_ROOT};${LIBXML2_ROOT};${ZSTD_ROOT}"
+  "../$PKGNAME-$PKGVERSION"
+  -G Ninja
+  -DCMAKE_BUILD_TYPE="${cmake_build_type}"
+  -DLLVM_BUILD_TYPE="${LLVM_BUILD_TYPE}"
+  -DCMAKE_INSTALL_PREFIX="${i}"
+  -DCMAKE_C_COMPILER=gcc
+  -DCMAKE_CXX_COMPILER=g++
+  -DCMAKE_Fortran_COMPILER=gfortran
+  -DCMAKE_LINKER=ld
+  -DCMAKE_VERBOSE_MAKEFILE=TRUE
+  -Droot7=ON
+  -Dfail-on-missing=ON
+  -Dgnuinstall=OFF
+  -Droofit=ON
+  -Dvdt=OFF
+  -Dhdfs=OFF
+  -Dqt=OFF
+  -Dtmva=ON
+  -DPython3_EXECUTABLE="${PYTHON3_ROOT}/bin/python3"
+  -Dqtgsi=OFF
+  -Dpgsql=OFF
+  -Dsqlite=OFF
+  -Dmysql=OFF
+  -Doracle=OFF
+  -Dldap=OFF
+  -Dkrb5=OFF
+  -Dftgl=OFF
+  -Dfftw3=ON
+  -Dtbb=ON
+  -Dimt=ON
+  -DFFTW_INCLUDE_DIR="${FFTW3_ROOT}/include"
+  -DFFTW_LIBRARY="${FFTW3_ROOT}/lib/libfftw3.${soext}"
+  -Dmathmore=ON
+  -Dexplicitlink=ON
+  -Dbuiltin_tbb=OFF
+  -Dbuiltin_pcre=OFF
+  -Dbuiltin_freetype=OFF
+  -Dbuiltin_zlib=OFF
+  -Dbuiltin_lzma=OFF
+  -Dbuiltin_gsl=OFF
+  -Dbuiltin_glew=ON
+  -Dbuiltin_ftgl=ON
+  -Dbuiltin_gl2ps=ON
+  -Dbuiltin_afterimage=ON
+  -Dbuiltin_xxhash=ON
+  -Dbuiltin_nlohmannjson=ON
+  -Darrow=OFF
+  -DGSL_ROOT_DIR="${GSL_ROOT}"
+  -DGSL_CBLAS_LIBRARY="${OPENBLAS_ROOT}/lib/libopenblas.${soext}"
+  -DGSL_CBLAS_LIBRARY_DEBUG="${OPENBLAS_ROOT}/lib/libopenblas.${soext}"
+  -DCMAKE_CXX_STANDARD="${cms_cxx_standard}"
+  -Dssl=ON
+  -Dpyroot=ON
+  -Dxrootd=ON
+  -Dbuiltin_xrootd=OFF
+  -DXROOTD_INCLUDE_DIR="${XROOTD_ROOT}/include/xrootd"
+  -DXROOTD_ROOT_DIR="${XROOTD_ROOT}"
+  -DCMAKE_C_FLAGS="-D__ROOFIT_NOBANNER"
+  -DCMAKE_CXX_FLAGS="-D__ROOFIT_NOBANNER"
+  -Dgviz=OFF
+  -Dbonjour=OFF
+  -Dodbc=OFF
+  -Dpythia6=OFF
+  -Dpythia8=OFF
+  -Dfitsio=OFF
+  -Dgfal=OFF
+  -Dchirp=OFF
+  -Dsrp=OFF
+  -Ddavix=ON
+  -Dglite=OFF
+  -Dsapdb=OFF
+  -Dalien=OFF
+  -Dmonalisa=OFF
+  -DJPEG_INCLUDE_DIR="${LIBJPEG_TURBO_ROOT}/include"
+  -DJPEG_LIBRARY="${LIBJPEG_TURBO_ROOT}/lib64/libjpeg.${soext}"
+  -DPNG_INCLUDE_DIRS="${LIBPNG_ROOT}/include"
+  -DPNG_LIBRARY="${LIBPNG_ROOT}/lib/libpng.${soext}"
+  -Dastiff=ON
+  -DTIFF_INCLUDE_DIR="${LIBTIFF_ROOT}/include"
+  -DTIFF_LIBRARY="${LIBTIFF_ROOT}/lib/libtiff.${soext}"
+  -DLIBLZMA_INCLUDE_DIR="${XZ_ROOT}/include"
+  -DLIBLZMA_LIBRARY="${XZ_ROOT}/lib/liblzma.${soext}"
+  -DLZ4_INCLUDE_DIR="${LZ4_ROOT}/include"
+  -DLZ4_LIBRARY="${LZ4_ROOT}/lib/liblz4.${soext}"
+  -DZLIB_ROOT="${ZLIB_ROOT}"
+  -DZLIB_INCLUDE_DIR="${ZLIB_ROOT}/include"
+  -DZSTD_ROOT="${ZSTD_ROOT}"
+  -DCMAKE_PREFIX_PATH="${LZ4_ROOT};${GSL_ROOT};${XZ_ROOT};${GIFLIB_ROOT};${FREETYPE_ROOT};${PYTHON3_ROOT};${LIBPNG_ROOT};${PCRE2_ROOT};${TBB_ROOT};${OPENBLAS_ROOT};${DAVIX_ROOT};${LIBXML2_ROOT};${ZSTD_ROOT}"
 )
 
 # Add OS-specific options
 if [ "$OS" = "linux" ]; then
-    cmake_args+=(
-        -Drfio=OFF
-        -Dcastor=OFF
-        -Ddcache=ON
-        -DDCAP_INCLUDE_DIR="${DCAP_ROOT}/include"
-        -DDCAP_DIR="${DCAP_ROOT}"
-    )
+  cmake_args+=(
+    -Drfio=OFF
+    -Dcastor=OFF
+    -Ddcache=ON
+    -DDCAP_INCLUDE_DIR="${DCAP_ROOT}/include"
+    -DDCAP_DIR="${DCAP_ROOT}"
+  )
 elif [ "$OS" = "darwin" ]; then
-    cmake_args+=(
-        -Dbuiltin_afterimage=OFF
-        -Dcocoa=OFF
-        -Dx11=ON
-        -Dcastor=OFF
-        -Drfio=OFF
-        -Ddcache=OFF
-    )
+  cmake_args+=(
+    -Dbuiltin_afterimage=OFF
+    -Dcocoa=OFF
+    -Dx11=ON
+    -Dcastor=OFF
+    -Drfio=OFF
+    -Ddcache=OFF
+  )
 fi
 
 # Execute cmake
 cmake "${cmake_args[@]}"
 
 for d in ${EXPAT_ROOT} ${BZ2LIB_ROOT} ${DB6_ROOT} ${GDBM_ROOT} ${LIBFFI_ROOT} ${ZLIB_ROOT} ${SQLITE_ROOT} ${XZ_ROOT} ${LIBUUID_ROOT}; do
-    if [[ -n "$d" ]]; then
-        if [[ -e "$d/lib" ]]; then
-            LDFLAGS="$LDFLAGS -L$d/lib"
-        fi
-        if [[ -e "$d/lib64" ]]; then
-            LDFLAGS="$LDFLAGS -L$d/lib64"
-        fi
-        if [[ -e "$d/include" ]]; then
-            CPPFLAGS="$CPPFLAGS -I$d/include"
-        fi
+  if [[ -n "$d" ]]; then
+    if [[ -e "$d/lib" ]]; then
+      LDFLAGS="$LDFLAGS -L$d/lib"
     fi
+    if [[ -e "$d/lib64" ]]; then
+      LDFLAGS="$LDFLAGS -L$d/lib64"
+    fi
+    if [[ -e "$d/include" ]]; then
+      CPPFLAGS="$CPPFLAGS -I$d/include"
+    fi
+  fi
 done
 
 for d in \
@@ -233,7 +234,8 @@ ninja -v ${JOBS:+-j$JOBS} install
 
 find $INSTALLROOT -type f -name '*.py' | xargs chmod -x
 grep -rlI '#!.*python' $INSTALLROOT | xargs chmod +x
-for p in $(grep -rlI -m1 '^#\!.*python' $INSTALLROOT/bin $INSTALLROOT/etc) ; do
+for p in $(grep -rlI -m1 '^#\!.*python' $INSTALLROOT/bin $INSTALLROOT/etc); do
   lnum=$(grep -n -m1 '^#\!.*python' $p | sed 's|:.*||')
   sed -i -e "${lnum}c#!/usr/bin/env python3" $p
 done
+
